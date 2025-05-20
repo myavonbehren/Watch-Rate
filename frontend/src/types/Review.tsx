bed0171a6d8c9c11fe50e8f9279209f61fac316d
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-20 06:59:39 +0000

    Create review type

---
+++ frontend/src/types/Review.tsx
@@ -0,0 +1,11 @@
+export interface Review {
+  id?: number;
+  username: string;
+  title: string;
+  content: string;
+  rating: number;
+  liked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  dueDate: string;
+}
