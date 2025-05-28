59bda4feaf675946b2fb94e19cedd62fdcc2fde9
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-28 23:11:20 +0000

    Update review model

--- backend/ReviewAPI/Models/Review.cs
+++ backend/ReviewAPI/Models/Review.cs
@@ -4,16 +4,12 @@ namespace ReviewAPI.Models;
 
 public class Review
 {
-    
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public int Rating { get; set; }
+    public Boolean Liked { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
-
-/*
-id?: number;
-  username: string;
-  title: string;
-  content: string;
-  rating: number;
-  liked: boolean;
-  createdAt: string;
-  updatedAt: string;
-*/
