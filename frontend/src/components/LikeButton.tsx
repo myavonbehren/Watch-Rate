924bd351bf9c641a0d5862552c1630dcaefcce5c
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-28 02:18:13 +0000

    Create like button props

---
+++ frontend/src/components/LikeButton.tsx
@@ -0,0 +1,14 @@
+import { Heart, HeartFill } from "react-bootstrap-icons";
+import React from "react";
+
+interface LikeButtonProps {
+    isLiked: boolean;
+    onToggle: () => void
+}
+
+const LikeButton: React.FC<LikeButtonProps> = ({isLiked, onToggle}) => {
+    return (
+
+    );
+};
+export default LikeButton;
