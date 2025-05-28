d5fca77ea36863029e8b65e86e3229d957e81d29
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-28 23:07:06 +0000

    Create review model

---
+++ backend/ReviewAPI/Models/Review.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ReviewAPI.Models;
+
+public class Review
+{
+    
+}
+
+/*
+id?: number;
+  username: string;
+  title: string;
+  content: string;
+  rating: number;
+  liked: boolean;
+  createdAt: string;
+  updatedAt: string;
+*/
