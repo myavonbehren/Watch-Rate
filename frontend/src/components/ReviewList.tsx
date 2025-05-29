6a47c939bc57fe5b2bd9ee97b61d4b8987fea971
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-29 00:49:34 +0000

    Create review list ui

--- frontend/src/components/ReviewList.tsx
+++ frontend/src/components/ReviewList.tsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
 import { type Review } from '../types/Review'
 import { reviewAPI } from '../services/reviewAPI'
+import { Form, Button, Card, FormGroup, CardTitle, Row, Col} from 'react-bootstrap';
+import StarRating from "./StarRating";
+import LikeButton from "./LikeButton";
+
 
 const ReviewList = () => {
+    {/*
     const [reviews, setReviews] = useState<Review[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
@@ -20,6 +25,39 @@ const ReviewList = () => {
         };
         fetchReviews();
     }, []);
+    */}
 
+    return (
+        <Card className="p-0 shadow-sm border" style={{  width: '40rem'  }}>
+            <Card.Body>
+                <CardTitle className="mt-4" style={{fontWeight: "bold", fontSize: "1.4em"}}>Reviews</CardTitle>
+                <FormGroup className='mb-4'>
+                <Row>
+                    <Col>
+                    <Card.Text>Title</Card.Text>
+                    </Col>
+                    <Col>
+                    <Card.Text>Date</Card.Text>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <StarRating rating ={3}></StarRating>
+                    </Col>
+                    <Col>
+                    <LikeButton isLiked={true}></LikeButton>
+                    </Col>
+                </Row>
+                
 
-}
+            </FormGroup>    
+            </Card.Body>      
+        </Card>
+
+    );
+
+    
+
+};
+
+export default ReviewList;
