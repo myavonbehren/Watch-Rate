91120716491283f2eb843249743d834f16dccfba
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-29 05:28:08 +0000

    Refactor ReviewItem component to use review data for rendering

--- frontend/src/components/ReviewItem.tsx
+++ frontend/src/components/ReviewItem.tsx
@@ -1,38 +1,41 @@
-import { useState, useEffect } from "react";
 import { type Review } from '../types/Review'
-import { reviewAPI } from '../services/reviewAPI'
 import { Button, Card, CardTitle, Row, Col, Stack} from 'react-bootstrap';
 import StarRating from "./StarRating";
 import LikeButton from "./LikeButton";
 
-const ReviewList = () => {
-    return (
+interface ReviewItemProps {
+    review: Review;
+    deleteReview: (id: number) => void;
+}
+
+const ReviewItem: React.FC<ReviewItemProps> = ({ review, deleteReview }) => {
+    return (        
         <Card className="p-0 shadow-sm border" style={{ width: '95vw', maxWidth: '20rem'}}>
-            <Card.Header className="bg-white">Username</Card.Header>
+            <Card.Header className="bg-white">{review.username}</Card.Header>
             <Card.Body className="text-start">
                 <Row className="align-items-center">
-                    <Col><CardTitle style={{fontSize: "1.2em"}}>Title</CardTitle></Col>
+                    <Col><CardTitle style={{fontSize: "1.2em"}}>{review.title}</CardTitle></Col>
                     <Col className="text-end"><Card.Subtitle className="mb-2 text-muted">Date</Card.Subtitle></Col>
                 </Row>
                 <Row className="mb-1">
                     <Col>
-                    <StarRating rating ={3} size={15}></StarRating>
+                    <StarRating rating ={review.rating} size={15}></StarRating>
                     </Col>
                     <Col className="text-end">
-                    <LikeButton isLiked={true} size={15}></LikeButton>
+                    <LikeButton isLiked={review.liked} size={15}></LikeButton>
                     </Col>
                 </Row>
 
-                <Card.Text>Content</Card.Text>
+                <Card.Text>{review.content}</Card.Text>
                 
             </Card.Body> 
             <Card.Footer className="d-flex justify-content-end" style={{ backgroundColor: '#f8f9fa' }}>
-                <Stack direction="horizontal" gap={2} >
-                    <Button variant="outline-primary" size="sm"> Edit </Button>
-                    <Button variant="danger" size="sm"> Delete </Button>
+                <Stack direction="horizontal" gap={2}>
+                    <Card.Link href={`/edit/${review.id}`}><Button variant="outline-primary" size="sm"> Edit </Button></Card.Link>
+                    <Button variant="danger" size="sm" onClick={() => review.id && deleteReview(review.id)}> Delete </Button>
                 </Stack>
             </Card.Footer>     
         </Card>
     );
 };
-export default ReviewList;
+export default ReviewItem;
