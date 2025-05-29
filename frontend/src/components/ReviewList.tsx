cc352504a3793a7ebbcc7fb1714829a80f16a965
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-29 05:28:37 +0000

    Refactor ReviewList component to remove unused imports

--- frontend/src/components/ReviewList.tsx
+++ frontend/src/components/ReviewList.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
 import { type Review } from '../types/Review'
 import { reviewAPI } from '../services/reviewAPI'
-import { Form, Button, Card, FormGroup, CardTitle, Row, Col} from 'react-bootstrap';
+import { Button, Card, CardTitle} from 'react-bootstrap';
 import ReviewItem from "./ReviewItem";
 
 const ReviewList = () => {
-    {/*
     const [reviews, setReviews] = useState<Review[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
@@ -17,25 +16,48 @@ const ReviewList = () => {
                 setReviews(data);
                 setLoading(false);
             } catch (err) {
+                console.error('Error: ', err)
                 setError('Failed to fetch reviews');
                 setLoading(false);
             }
         };
         fetchReviews();
     }, []);
-    */}
+    
+    const deleteReview = async (id: number) => {
+        if (window.confirm('Are you sure you want to delete this review?')) {
+            try {
+                await reviewAPI.delete(id);
+                setReviews(reviews.filter(review => review.id != id));
+            } catch (err) {
+                console.error('Error deleting review: ', error)
+            }
+        }
+    };
+
+    if (loading) return <div> Loading... </div>;
+    if (error) return <div> Error: {error} </div>;
 
     return (
-        <Card className="p-0 shadow-sm border mx-auto" style={{ width: '95vw', maxWidth: '40rem'}}>
-            <Card.Body>
-                <CardTitle className="mt-4" style={{fontWeight: "bold", fontSize: "1.4em"}}>Reviews</CardTitle>
-            <ReviewItem></ReviewItem>
-            </Card.Body>
-        </Card>
+        <div className="container-fluid">
+            <Card className="p-0 shadow-sm border mx-auto" style={{ width: '95vw', maxWidth: '40rem'}}>
+                <Card.Body>
+                    <CardTitle className="mt-4" style={{fontWeight: "bold", fontSize: "1.4em"}}>Reviews</CardTitle>
+                    <Card.Link href="/add"><Button className="mt-2">Add Review</Button></Card.Link>
+                </Card.Body>
+            </Card>
+
+            {reviews.length === 0 ? (
+                <p>No reviews available</p>
+            ) : (
+                reviews.map((review) => (
+                    <ReviewItem
+                    review={review}
+                    deleteReview={deleteReview}></ReviewItem>
+                ))
+            )}
+        </div>
     );
-
-    
-
 };
 
 export default ReviewList;
