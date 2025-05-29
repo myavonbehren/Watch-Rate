b838f7875f222d2dd670e6b918a33b613d986ad0
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-29 04:05:06 +0000

    Add background color to card header, add buttons to footer

--- frontend/src/components/ReviewItem.tsx
+++ frontend/src/components/ReviewItem.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { type Review } from '../types/Review'
 import { reviewAPI } from '../services/reviewAPI'
-import { Form, Button, Card, FormGroup, CardTitle, Row, Col} from 'react-bootstrap';
+import { Button, Card, CardTitle, Row, Col, Stack} from 'react-bootstrap';
 import StarRating from "./StarRating";
 import LikeButton from "./LikeButton";
 
 const ReviewList = () => {
     return (
         <Card className="p-0 shadow-sm border" style={{ width: '95vw', maxWidth: '20rem'}}>
-            <Card.Header>Username</Card.Header>
+            <Card.Header className="bg-white">Username</Card.Header>
             <Card.Body className="text-start">
                 <Row className="align-items-center">
                     <Col><CardTitle style={{fontSize: "1.2em"}}>Title</CardTitle></Col>
@@ -25,9 +25,13 @@ const ReviewList = () => {
 
                 <Card.Text>Content</Card.Text>
                 
-                <Button variant="primary" size="sm"> Edit </Button>
-                <Button variant="danger" size="sm"> Delete </Button>
-            </Card.Body>      
+            </Card.Body> 
+            <Card.Footer className="d-flex justify-content-end" style={{ backgroundColor: '#f8f9fa' }}>
+                <Stack direction="horizontal" gap={2} >
+                    <Button variant="outline-primary" size="sm"> Edit </Button>
+                    <Button variant="danger" size="sm"> Delete </Button>
+                </Stack>
+            </Card.Footer>     
         </Card>
     );
 };
