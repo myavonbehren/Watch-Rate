d9651a25b1c76a7b8641b59f8a9b90a171b21c6a
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-05-29 01:19:39 +0000

    Fix margin top spacing

--- frontend/src/components/ReviewItem.tsx
+++ frontend/src/components/ReviewItem.tsx
@@ -10,7 +10,7 @@ const ReviewList = () => {
         <Card className="p-0 shadow-sm border mx-auto" style={{ width: '95vw', maxWidth: '20rem'}}>
             <Card.Header>Username</Card.Header>
             <Card.Body className="text-start">
-                <CardTitle className="mt-4" style={{fontSize: "1.2em"}}>Title</CardTitle>
+                <CardTitle style={{fontSize: "1.2em"}}>Title</CardTitle>
                 <Card.Subtitle className="mb-2 text-muted">Date</Card.Subtitle>
                 <Card.Text>Content</Card.Text>
                 <Row>
