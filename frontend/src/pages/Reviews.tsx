6023ac2e5cc70764e98aa3bfd297e0bb3d6706ed
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-06-02 08:11:41 +0000

    Add Reviews component with review list and add review functionality

---
+++ frontend/src/pages/Reviews.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { type Review } from '../types/Review'
+import ReviewList from '../components/ReviewList.tsx';
+import AddReview from '../components/AddReview.tsx'
+
+const Reviews: React.FC = () => {
+    const [reviews, setReviews] = useState<Review[]>();
+
+   const addReview = (newReview: Review) => {
+      console.log('Adding review:', newReview);
+    };
+
+    return (
+        <>
+      <div>
+        <AddReview addReview={(addReview)} />
+        <ReviewList></ReviewList>
+      </div>
+    </>
+    );
+
+
+};
+
+export default Reviews;
