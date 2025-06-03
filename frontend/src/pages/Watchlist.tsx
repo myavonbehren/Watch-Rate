e1056f3ecd4becd75708286777be002b5f4d4c1c
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-06-03 19:19:59 +0000

    Add Watchlist component with basic structure

---
+++ frontend/src/pages/Watchlist.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Watchlist: React.FC = () => {
+    return (
+        <div>
+            <h1>Watchlist</h1>           
+        </div>
+    );
+};
+
+export default Watchlist;
