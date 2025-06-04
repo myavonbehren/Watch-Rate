2d13397f4f5d410d35c8eac50b223c67ed76dab3
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-06-04 00:57:34 +0000

    Refactor Watchlist component layout to include AddShow and ShowList components

--- frontend/src/pages/Watchlist.tsx
+++ frontend/src/pages/Watchlist.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import AddShow from '../components/AddShow';
+import ShowList from '../components/ShowList';
 
 const Watchlist: React.FC = () => {
     return (
         <div>
-            <h1>Watchlist</h1>           
+            <h1>Watchlist</h1>
+            <div className="d-flex flex-wrap gap-4 justify-content-center">
+                <div className='flex-shrink-0'>
+                    <AddShow/>
+                </div>
+                <div className="flex-grow-1" style={{ minWidth: '300px' }}>
+                    <ShowList/>
+                </div>
+            </div>
         </div>
     );
 };
