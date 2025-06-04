9b414393ee6cec6fe90309e6cb45afc345e75fe7
Mya Von Behren <105401477+myavonbehren@users.noreply.github.com>
2025-06-04 06:40:23 +0000

    add showAPI service for managing shows

---
+++ frontend/src/services/showAPI.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { type Show } from '../types/Show'
+
+const API_URL = 'http://localhost:5023'
+
+export const showAPI = {
+  getAll: async (): Promise<Show[]> => {
+    const response = await axios.get<Show[]>(`${API_URL}/shows`);
+    return response.data;
+  },
+
+  getById: async (id: number): Promise<Show> => {
+    const response = await axios.get<Show>(`${API_URL}/shows/${id}`);
+    return response.data;
+  },
+
+  create: async (show: Show): Promise<Show> => {
+    const response = await axios.post<Show>(`${API_URL}/shows`, show);
+    return response.data;
+  },
+  
+  updateWatched: async (id: number, isAvailable: boolean): Promise<void> => {
+    await axios.patch(`/shows/${id}/watched?isWatched=${isAvailable}`);
+  },
+
+  delete: async (id: number): Promise<void> => {
+    await axios.delete(`${API_URL}/shows/${id}`);
+  }
+};
